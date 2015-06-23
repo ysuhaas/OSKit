8feaa82d8e97fc5b4b2d45e4996cae0eb233e00a
ysuhaas <ysuhaas@gmail.com>
2015-06-23 15:52:03 -0500

    Upload
    Upload is finally functional (fileKey issue resolved). Purge cache
    feature also implemented (untested as of yet). Each Session ID now
    creates a separate directory, which can be purged in the settings.
    Still need to fix passing of arguments to write the proper data to file.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,9 +7,9 @@ angular.module('starter.controllers', [])
   //$scope.$on('$ionicView.enter', function(e) {
   //});
 
-
 .controller('DashCtrl', function($scope) {
  
+ //Connection Status Function
   $scope.checkConnection = function () {
       console.log('Click');
       var networkState = navigator.connection.type;
@@ -25,10 +25,12 @@ angular.module('starter.controllers', [])
 
             window.alert('Connection type: ' + states[networkState]);
         };
+
+
 })
 
 
-.controller('ChatsCtrl', function($scope, $cordovaGeolocation, $cordovaFile, $cordovaFileTransfer, $ionicPlatform) {
+.controller('ChatsCtrl', function($scope, $cordovaGeolocation, $cordovaFile, $cordovaFileTransfer, $ionicPlatform, $timeout) {
 
 //Defining Input Fields
   $scope.temp = {};
@@ -37,10 +39,10 @@ angular.module('starter.controllers', [])
   $scope.weather = {};
   $scope.userSID = {};
   $scope.a = {
-    temp:1, 
-    salin:2, 
-    sechi:3, 
-    weather:4
+    temp:0, 
+    salin:1, 
+    sechi:2, 
+    weather:3
   };
   $scope.loc = {};
   
@@ -48,18 +50,19 @@ angular.module('starter.controllers', [])
 //Updates the Session ID and Retrieves Location Data
   $scope.getSID = function () {
     var SID = $scope.userSID.data;
-    console.log('Your session ID is ' + SID);
+    console.log('Session ID set to: ' + SID);
     window.alert ('Your new session ID is ' + SID);
     var pos0ptions = {timeout: 10000, enableHighAccuracy: true};
-    $cordovaGeolocation.getCurrentPosition(pos0ptions).then(function(position){
-      console.log('Latitude: '    + position.coords.latitude           + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
+    $cordovaGeolocation.getCurrentPosition(pos0ptions)
+      .then(function(position){
+        console.log('Latitude: '    + position.coords.latitude           + '\n' +
+            'Longitude: '         + position.coords.longitude         + '\n' +
+            'Altitude: '          + position.coords.altitude          + '\n' +
+            'Accuracy: '          + position.coords.accuracy          + '\n' +
+            'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+            'Heading: '           + position.coords.heading           + '\n' +
+            'Speed: '             + position.coords.speed             + '\n' +
+            'Timestamp: '         + position.timestamp                + '\n');
 
       $scope.loc.lat = position.coords.latitude
       $scope.loc.lon = position.coords.longitude
@@ -77,92 +80,121 @@ angular.module('starter.controllers', [])
 
 
 //Parent export function: Should save file, tick up counter, and clear field
-  $scope.doSave = function (a) {
-   $ionicPlatform.ready(function(a) {
+  $scope.doSave = function (e) {
+   $ionicPlatform.ready(function(e) {
       var SID = $scope.userSID.data;
-    
-      if (a == 1) {
-        var data1 = "temp.data + ', ' + temp.depth";
-        var variable = "temperature";
-        document.getElementById("counter-1").innerHTML = 1
-      }
-       if (a == 2) {
-        var data1 = "salin.data + ', ' + salin.depth";
-        var variable = "salinity";
-        document.getElementById("counter-2").innerHTML = 1
-      }
-       if (a = "3") {
-        var data1 = "sechi.depth";
-        var variable = "sechi";
-        document.getElementById("counter-3").innerHTML = 1
-      }
-       if (a = "4") {
-        var data1 = "weather.data";
-        var variable = "weather";
-        document.getElementById("counter-4").innerHTML = 1
-      }
-
-      var DTG = Date.now()
-      //var fileName = SID + "_" + variable + "_" + DTG + ".oskit";
-      var fileName = "test.txt"
-      console.log("FILENAME: " + fileName);
-      console.log(data1);
-
-
-
-      $cordovaFile.writeFile(cordova.file.documentsDirectory, fileName, data1, true)
+      var DTG = Date.now();
+      $cordovaFile.createDir(cordova.file.documentsDirectory, SID, false)
       .then(function (success) {
-        console.log('File successfully saved.');
-        console.log(success);
-        $cordovaFile.checkFile(cordova.file.documentsDirectory, fileName)
-        .then(function (success) {
-          console.log("File found.")
-
-        }, function (error) {
-          console.log("File not found");
-          });
-
-        
+        console.log("New directory " + SID + " created.");
       }, function (error) {
+        window.alert("Error: This Session ID has already been used.");
         console.log(error);
+      });
+      
+      switch (e) {
+        case 0:
+          var data = "temp.data + ', ' + temp.depth";
+          var variable = "temperature";
+          document.getElementById("counter-1").innerHTML = 1
+          break;
+        case 1:
+          var data = "salin.data + ', ' + salin.depth";
+          var variable = "salinity";
+          document.getElementById("counter-2").innerHTML = 1
+          break;
+        case 2:
+          var data = "sechi.depth";
+          var variable = "sechi";
+          document.getElementById("counter-3").innerHTML = 1
+          break;
+        case 3:
+          var data = "weather.data";
+          var variable = "weather";
+          document.getElementById("counter-4").innerHTML = 1
+          break;
+      }
+      //next 2 lines for debugging only
+    console.log("data field: " + variable);
+    console.log("data: " + data);
+    $scope.fileName = SID + "_" + variable + "_" + DTG + ".oskit";
+    
+    $cordovaFile.writeFile(cordova.file.documentsDirectory + SID, fileName, data, false)
+    .then(function (success) {
+      console.log('File successfully saved.');
+      //console.log(success);
+
+      //Used to check if file has actually been created, should not be needed for final build.
+        //$cordovaFile.checkFile(cordova.file.documentsDirectory, fileName)
+        //.then(function (success) {
+          //console.log("File found.")
+        //}, function (error) {
+          //console.log("File not found");
+          //});
+
+      }, function (error) {
+        console.log("File not saved");
+        console.log("ERROR: " + error);
       });
 
     })
   };
 
 
-
 // Upload function: Should upload all files in directory
-$scope.doUpload = function() {
-console.log("click");
-     var server = "http://www7330.nrlssc.navy.mil/derada/AEC/upload.php";
-      var options = {
-          fileName: "test.txt",
-          httpMethod: "POST",
-          chunkedMode: false,
-          mimeType: "text/plain"
-      };
+  $scope.doUpload = function() {
+    var url = "http://www7330.nrlssc.navy.mil/derada/AEC/upload.php";
+    var targetPath = cordova.file.documentsDirectory + SID + "filename.txt";
+    var filesname = targetPath.split("/").pop();
+    var options = {
+      fileKey: "filename",
+      fileName: filesname,
+      chunkedMode: false,
+      mimeType: "text/plain"
+    };
 
-      var trustAllHosts = true
-      var filePath = cordova.file.documentsDirectory + "test.txt"
+    $cordovaFileTransfer.upload(url, targetPath, options)
+    .then(function(result) {
+      console.log("SUCCESS: " + JSON.stringify(result.response));
+      window.alert("Files successfully uploaded!");
+      console.log(JSON.stringify(result.response));
+    }, function(err) {
+      console.log("ERROR: " + JSON.stringify(err));
+      window.alert(JSON.stringify(err));
+      //Can add error codes/handlers here
+    }, function (progress) {
+      $timeout(function () {
+      $scope.downloadProgress = (progress.loaded / progress.total) * 100;
+      })
+    });  
+  }
 
-$cordovaFileTransfer.upload(server, filePath, options)
-      .then(function(result) {
-            console.log(success);
-            }, function(err) {
-            console.log(err);
-            }, function (progress) {
-              console.log(progress);
-            });
-};
 })
 
 
 
-
-
-.controller('AccountCtrl', function($scope, $cordovaGeolocation) {
+.controller('AccountCtrl', function($scope, $cordovaDialogs, $cordovaFile) {
 $scope.doRemove = function() {
-  window.alert("Local cache cleared.");
+  $cordovaDialogs.confirm('Are you sure you want to delete all local files? This action is permanant.', 'Purge Local Cache', ['Delete','Cancel'])
+    .then(function(buttonIndex) {
+      // no button = 0, 'OK' = 1, 'Cancel' = 2
+      var btnIndex = buttonIndex;
+    });
+
+  if (buttonIndex == 1) {
+    $ionicPlatform.ready(function() {
+      $cordovaFile.removeDir(cordova.file.documentsDirectory, SID)
+      .then(function (success) {
+        window.alert("Files successfully deleted.");
+      }, function (error) {
+        window.alert("Error deleting files.");
+        console.log(error);
+      });  
+    });
+  }
+  else {
+    return null;
+  }
+
 }  
 })
